eaaadb5c31b9bc71d96e0e81de69ba81cad3c72f
Indo86 <deninorman77@gmail.com>
2025-05-27 18:54:44 +0700

    update: db gcp

--- config/database.js
+++ config/database.js
@@ -6,8 +6,9 @@ dotenv.config();
 const DB_NAME = process.env.DB_NAME;
 const DB_USERNAME= process.env.DB_USERNAME;
 const DB_HOST= process.env.DB_HOST;
+const DB_PASSWORD= process.env.DB_PASSWORD;
 
-const db = new Sequelize(DB_NAME, DB_USERNAME, "", {
+const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql"
 });
